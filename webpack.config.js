e99a8149a4f905e677b3b8b01ae93177d1578741
sk <semaria.eyzac@gmail.com>
2019-05-24 21:36:39 -0700

    install the uglifying, cleanwebpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack');
 
 module.exports = {
   entry: './src/main.js',
@@ -8,6 +10,8 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
+    new UglifyJsPlugin(),
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Galactic Age Calculator',
       template: './src/index.html',
